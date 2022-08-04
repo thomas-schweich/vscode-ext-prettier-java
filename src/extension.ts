e5ee0d9db5ca99934bd122a14f821c45d980d8dc
thomas.schweich <thomas@hyperproof.io>
2022-08-04 13:03:13 -0700

    Convert to typescript and configure debugging.

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,30 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-const prettier = require("prettier");
+import * as vscode from 'vscode';
+const prettier = require('prettier');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-  let disposable = vscode.languages.registerDocumentFormattingEditProvider(
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Starting Prettier for Java VSCode extension from Hyperproof.')
+  const disposable = vscode.languages.registerDocumentFormattingEditProvider(
     "java",
     {
       async provideDocumentFormattingEdits(document) {
         const text = document.getText();
-        const options = await prettier.resolveConfig(document.fileName, {
-          editorconfig: true,
-        });
+        const options = await prettier.resolveConfig(document.fileName);
         const formattedText = prettier.format(text, {
           ...options,
           parser: "java"
         });
 
-        let textEditor = vscode.window.activeTextEditor;
+        const textEditor = vscode.window.activeTextEditor;
         if (textEditor) {
-          let firstLine = textEditor.document.lineAt(0);
-          let lastLine = textEditor.document.lineAt(
+          const firstLine = textEditor.document.lineAt(0);
+          const lastLine = textEditor.document.lineAt(
             textEditor.document.lineCount - 1
           );
           let textRange = new vscode.Range(
@@ -44,12 +41,7 @@ function activate(context) {
 
   context.subscriptions.push(disposable);
 }
-exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {}
 
 module.exports = {
-  activate,
-  deactivate,
+  activate
 };
